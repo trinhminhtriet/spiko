055017d660f9b8644b66e9f63cf830a9bbad7629
Trinh Minh Triet <contact@trinhminhtriet.com>
2024-11-10 22:19:09 +0700

    bump version to 0.1.3 and update release script to increase sleep duration

--- scripts/release-version.sh
+++ scripts/release-version.sh
@@ -16,7 +16,7 @@ echo ">>> Bumping version"
 sed -i.bak "s/version = \"$current_version\"/version = \"$new_version_num\"/" Cargo.toml
 rm Cargo.toml.bak
 
-sleep 2
+sleep 30
 
 echo ">>> Commit"
 git add Cargo.toml Cargo.lock
