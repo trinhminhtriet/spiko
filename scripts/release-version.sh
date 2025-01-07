2ba2b4f9b78ac2115118c505ce9a5da6deb15414
Trinh Minh Triet <contact@trinhminhtriet.com>
2025-01-07 17:44:36 +0700

    fix(release): automate version bumping to the next patch version

--- scripts/release-version.sh
+++ scripts/release-version.sh
@@ -3,9 +3,11 @@ set -xe
 
 [ -z "$(git status --porcelain)" ] || (echo "dirty working directory" && exit 1)
 
-current_version="$(grep '^version = ' Cargo.toml | head -1 | cut -d '"' -f2)"
-new_version="$1"
-new_version_num="${new_version#"v"}"
+current_version="$(grep '^version = ' safeshell/Cargo.toml | head -1 | cut -d '"' -f2)"
+IFS='.' read -r major minor patch <<<"$current_version"
+new_patch=$((patch + 1))
+new_version="$major.$minor.$new_patch"
+tag_name="v$new_version"
 
 if [ -z "$new_version" ]; then
     echo "New version required as argument"
@@ -13,16 +15,16 @@ if [ -z "$new_version" ]; then
 fi
 
 echo ">>> Bumping version"
-sed -i.bak "s/version = \"$current_version\"/version = \"$new_version_num\"/" Cargo.toml
-rm Cargo.toml.bak
+sed -i.bak "s/version = \"$current_version\"/version = \"$new_version\"/" safeshell/Cargo.toml
+rm safeshell/Cargo.toml.bak
 
 sleep 5
 
 echo ">>> Commit"
-git add Cargo.toml Cargo.lock
+git add safeshell/Cargo.toml Cargo.lock
 git commit -am "version $new_version"
-git tag $new_version
+git tag $tag_name
 
 echo ">>> Publish"
 git push
-git push origin $new_version
+git push origin $tag_name
