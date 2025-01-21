836d0851c36da5a9e2c0404470f82733006a1707
Trinh Minh Triet <contact@trinhminhtriet.com>
2025-01-21 09:35:55 +0700

    fix(release): update version script to reference correct Cargo.toml path

--- scripts/release-version.sh
+++ scripts/release-version.sh
@@ -3,7 +3,7 @@ set -xe
 
 [ -z "$(git status --porcelain)" ] || (echo "dirty working directory" && exit 1)
 
-current_version="$(grep '^version = ' safeshell/Cargo.toml | head -1 | cut -d '"' -f2)"
+current_version="$(grep '^version = ' Cargo.toml | head -1 | cut -d '"' -f2)"
 IFS='.' read -r major minor patch <<<"$current_version"
 new_patch=$((patch + 1))
 new_version="$major.$minor.$new_patch"
@@ -15,13 +15,13 @@ if [ -z "$new_version" ]; then
 fi
 
 echo ">>> Bumping version"
-sed -i.bak "s/version = \"$current_version\"/version = \"$new_version\"/" safeshell/Cargo.toml
-rm safeshell/Cargo.toml.bak
+sed -i.bak "s/version = \"$current_version\"/version = \"$new_version\"/" Cargo.toml
+rm Cargo.toml.bak
 
 sleep 5
 
 echo ">>> Commit"
-git add safeshell/Cargo.toml Cargo.lock
+git add Cargo.toml Cargo.lock
 git commit -am "version $new_version"
 git tag $tag_name
 
